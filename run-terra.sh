3a810c563bd57d52fed1ae806e3721ab10a96bb1
ibrez <ibrezm1@gmail.com>
2022-01-13 00:11:49 +0530

    initial

--- run-terra.sh
+++ run-terra.sh
@@ -9,3 +9,6 @@ terraform plan -out terraform.out
 (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
 terraform apply terraform.out
 (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
+
+publicip=$(terraform output -json | jq ".instance_public_ip.value" | tr -d '"')
+ssh -i virginia-region-key-pair ec2-user@$publicip
